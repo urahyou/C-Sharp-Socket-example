5a5681ecb11156485bc429e711700435f1e3e4ca
urahyou <2726717369@qq.com>
2022-07-19 18:25:42 +0800

    socket通信

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+namespace socket_client
+{
+    class Program
+    {
+        static Socket clientSocket;
+
+        static void Main(string[] args)
+        {
+            
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            //连接
+            Connect(); 
+        }
+
+        //连接客户端，连接后开始监听发送过来的消息并且把输入的信息发给服务端
+        static void Connect()
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+            
+            try
+            {
+                //连接
+                clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
+
+                //独立线程来接受来自服务端的数据
+                Thread receive = new Thread(Receive);
+                receive.Start(clientSocket);
+
+                //独立线程来发送数据给服务端
+                Thread send = new Thread(Send);
+                send.Start(clientSocket); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Receive(object so)
+        {
+            Socket clientSocket = so as Socket;
+
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;  // 修改文字颜色为绿色
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int len = clientSocket.Receive(buffer);
+                    if (len > 0)
+                    {
+                        string msg = Encoding.UTF8.GetString(buffer);
+                        
+                        Console.WriteLine("服务端说："+ msg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        static void Send(object so)
+        {
+            Socket clientSocket = so as Socket;
+
+            while (true)  // 监听键盘输入
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                //获取键盘输入
+                string input = Console.ReadLine();
+                //编码
+                byte[] buffer = Encoding.UTF8.GetBytes(input);
+                //发送
+                clientSocket.Send(buffer);
+            }
+        }
+    }
+}
---
+++ MyServer/MyServer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace socket
+{
+    class Program
+    {
+
+        static Socket serverSocket;
+       
+        
+
+        static void Main(string[] args)
+        {
+            SetListen();  //开始监听
+
+        }
+
+        static void SetListen()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
+                serverSocket.Listen(2);
+
+                Thread thread = new Thread(Listen);
+                thread.Start(serverSocket);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        static void Listen(object so)
+        {
+            Socket serverSocket = so as Socket;
+
+            while (true)
+            {
+                try
+                {
+                    Socket clientSocket = serverSocket.Accept(); //接受客户端接入
+                    // 获取链接IP地址
+                    string clientPoint = clientSocket.RemoteEndPoint.ToString();
+
+                    //开启新线程来不停接受信息
+                    Thread rec = new Thread(Receive);
+                    rec.Start(clientSocket);
+
+                    //开启新线程来不停发送信息
+                    Thread sen = new Thread(Send);
+                    sen.Start(clientSocket);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message, ex);
+                }
+            }
+        }
+
+        static void Receive(object so)
+        {
+            Socket clientSocket = so as Socket;
+
+            string clientPoint = clientSocket.RemoteEndPoint.ToString();
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int len = clientSocket.Receive(buffer);
+                    //Console.WriteLine(len.ToString(), buffer);
+                    if (len == 0) break;
+
+                    string msg = Encoding.UTF8.GetString(buffer, 0, len);
+                    Console.WriteLine("客户端说："+ msg);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        static void Send(object so)
+        {
+            Socket clientSocket = so as Socket;
+
+            while (true)
+            {
+                //获取控制台输入
+                string input = Console.ReadLine();
+
+                byte[] msg = Encoding.UTF8.GetBytes(input);
+                clientSocket.Send(msg);
+                
+            }
+        }
+    }
+}
